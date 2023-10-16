96c819c9170acf8d93fde586f3564ec5b6fffba1
AfkarMohammed <afkarmohammed356@gmail.com>
2023-10-16 09:24:49 +0530

    Initial commit

---
+++ LP/calculator/src/routes.php
@@ -0,0 +1,7 @@
+<?php
+Route::get('calculator',function(){
+    echo"Hello from calculator package";
+});
+
+Route::get('add/{a}/{b}',[LP\Calculator\CalculatorController::class,'add']);
+Route::get('substract/{a}/{b}',[LP\Calculator\CalculatorController::class,'substract']);
